3f7e6f4c0048a134aafe1683512e2ebbe15c74b7
febriardis <ardisaputrafebri@gmail.com>
2021-12-22 15:26:52 +0700

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <swiper :slides-per-view="3" :space-between="50" @swiper="onSwiper" @slideChange="onSlideChange">
+    <swiper-slide>Slide 1</swiper-slide>
+    <swiper-slide>Slide 2</swiper-slide>
+    <swiper-slide>Slide 3</swiper-slide>
+  </swiper>
+</template>
+<script>
+  // Import Swiper Vue.js components
+  import { Swiper as SwiperClass } from 'swiper/core';
+  import getAwesomeSwiper from 'vue-awesome-swiper/dist/exporter'
+  // SwiperClass.use([Pagination, Mousewheel, Navigation, Autoplay])
+  const {Swiper, SwiperSlide, directive} = getAwesomeSwiper(SwiperClass)
+
+  import 'swiper/swiper-bundle.min.css'
+
+  export default {
+    components: {
+      Swiper,
+      SwiperSlide,
+    },
+    directives: {
+      swiper: directive
+    },
+    setup() {
+      const onSwiper = (swiper) => {
+        console.log(swiper);
+      };
+      const onSlideChange = () => {
+        console.log('slide change');
+      };
+      return {
+        onSwiper,
+        onSlideChange,
+      };
+    },
+  };
+</script>
